ec518261eed8da91f23faf9dc320330fbadb836b
Daria Viter <viter.daria@gmail.com>
2020-12-15 18:48:36 +0200

    Задание 1-6

---
+++ task-2.js
@@ -0,0 +1,32 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+  let messageSplitted = message.split(' ');
+  let numOfWords = messageSplitted.length;
+  console.log(numOfWords);
+  let messagePrice = numOfWords * pricePerWord;
+  console.log(messagePrice);
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
